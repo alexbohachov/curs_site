05bc85a0c3ae8610a2e64ddca30ee38147d4d80e
AlexBohachov <alexbogachev93@protonmail.com>
2021-04-11 15:40:50 +0300

    Lector_1_Django

--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,24 +1,53 @@
 from django.shortcuts import render, redirect
-from django.shortcuts import HttpResponse
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse
 from .models import Article
 from .forms import ArticleForm
 
 
-def index(request):
-    article = Article.objects.all()
-    return render(request, "index.html", {'articles': article})
+class IndexView(ListView):
+    model = Article
+    template_name = 'index.html'
+    context_object_name = 'articles'
+
+    def get_queryset(self):
+        return Article.objects.all()[:2]
+    
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IndexView, self).get_context_data()
+        return context
 
 
-def create(request):
-    form = ArticleForm()
-    if request.method == 'POST':
-        form = ArticleForm(request.POST)
-        if form.is_valid():
-            created = form.save()
-        return redirect('detail', created.id)
-    return render(request, 'article_templates/create.html', {'form': form})
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article_templates/create.html'
+    context_object_name = 'article'
+    form_class = ArticleForm
+
+    def get_success_url(self):
+        return reverse('detail', args=(self.object.id,))
 
 
-def detail(request, article_id):
-    article = Article.objects.get(id=article_id)
-    return render(request, 'article_templates/detail.html', {'article': article})
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_templates/detail.html'
+    context_object_name = 'article'
+    pk_url_kwarg = 'article_id'
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = 'article_templates/update.html'
+    form_class = ArticleForm
+    pk_url_kwarg = 'article_id'
+
+    def get_success_url(self):
+        return reverse('detail', args=(self.object.id,))
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    success_url = '/'
+    pk_url_kwarg = 'article_id'
+    template_name = 'article_templates/confirm_delete.html'
+    context_object_name = 'article'
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from article.views import index, create, detail
+from article.views import IndexView, ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('article/create', create, name='create'),
-    path('article/<int:article_id>', detail, name='detail'),
+    path('', IndexView.as_view(), name='index'),
+    path('article/create', ArticleCreateView.as_view(), name='create'),
+    path('article/<int:article_id>', ArticleDetailView.as_view(), name='detail'),
+    path('article/update/<int:article_id>', ArticleUpdateView.as_view(), name='update'),
+    path('article/delete/<int:article_id>', ArticleDeleteView.as_view(), name='delete'),
  ]
